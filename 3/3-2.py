f336dde0e41927e61876b0a71375d92a0b54d6f7
JW0827 <145167631+Jangwoo0827@users.noreply.github.com>
2025-01-16 13:56:16 +0000

    Commit

--- 3/3-2.py
+++ 3/3-2.py
@@ -1,4 +1,3 @@
-a = int(input())
 aa = ""
 for i in range(a):
     aa += "*"
---
+++ 3/w3.py
@@ -0,0 +1,31 @@
+l = list("python")
+print(l[0], l[2])
+
+lp = "24가 2210"
+print(lp[4:9])
+
+s = list("efefef")
+print(s[0]+s[2]+s[4])
+
+r = "PYTHON"[::-1]
+print(r)
+
+pn = "010-1234-5678".replace("-", " ")
+print(pn)
+
+pn = "010-1234-5678".replace("-", "")
+print(pn)
+
+url = "hi.com"
+print(url[3:6])
+
+la = list('python')
+la[0] = 'P'
+print(la)
+
+st = 'bababhbahbabh'.replace('a', 'A')
+print(st)
+
+string = 'abcd'
+string.replace('b', 'B')
+print(string)
